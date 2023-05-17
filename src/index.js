d0eaec1e0dddcf337d9e451d2dfbf973a0687bde
SaraTauriz <sarats214@live.com>
2023-05-17 13:25:20 -0500

    inventario

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 require("./config/passport");
 
 //Settings
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views")); //Carpeta de vistas
 app.engine(
 	".hbs",
